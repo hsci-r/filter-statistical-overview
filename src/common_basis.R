3850c22e287ce367e57c21fab1556451803f9441
Eetu Mäkelä <eetu.makela@iki.fi>
2023-10-06 15:00:16 +0300

    update project to use latest version of hscidbutil

--- src/common_basis.R
+++ src/common_basis.R
@@ -10,6 +10,7 @@ library(RMariaDB)
 library(sf)
 library(tmap)
 library(ggridges)
+library(ggrepel)
 
 p <- function(number) {
   return(format(number, scientific = FALSE, big.mark = ","))
@@ -21,43 +22,22 @@ is_html_output <- function() {
   is.null(knitr::pandoc_to()) || (!str_detect(knitr::pandoc_to(), "^gfm") && knitr::is_html_output())
 }
 
-while (!exists("con")) {
-  tryCatch(con <- dbConnect(
-    drv = MariaDB(),
-    host = "vm3634.kaj.pouta.csc.fi",
-    dbname = "filter_statistical_overview",
-    user = if (Sys.getenv("DB_USER")!="") Sys.getenv("DB_USER") else key_get("filter_overview","DB_USER"),
-    password = if (Sys.getenv("DB_PASS")!="") Sys.getenv("DB_PASS") else key_get("filter_overview","DB_PASS"),
-    bigint = "integer",
-    timeout = Inf,
-    load_data_local_infile = TRUE,
-    autocommit = TRUE,
-    reconnect = TRUE
-  ), error = function(e) {
-    print(e)
-    key_set("filter_overview","DB_USER", prompt="DB username: ")
-    key_set("filter_overview","DB_PASS", prompt="DB password: ")
-  })
-}
+con <- get_connection()
 
-poems <- tbl(con,dbplyr::in_schema("filter","poems"))
-poem_stats <- tbl(con,dbplyr::in_schema("filter","poem_stats"))
-p_year <- tbl(con,dbplyr::in_schema("filter","p_year"))
-verses <- tbl(con,dbplyr::in_schema("filter","verses"))
-verses_cl <- tbl(con,dbplyr::in_schema("filter","verses_cl"))
-verse_poem <- tbl(con,dbplyr::in_schema("filter","verse_poem"))
-collectors <- tbl(con,dbplyr::in_schema("filter","collectors"))
-p_col <- tbl(con,dbplyr::in_schema("filter","p_col"))
-locations <- tbl(con,dbplyr::in_schema("filter","locations"))
-p_loc <- tbl(con,dbplyr::in_schema("filter","p_loc"))
-themes <- tbl(con,dbplyr::in_schema("filter","themes"))
-poem_theme <- tbl(con,dbplyr::in_schema("filter","poem_theme"))
-refs <- tbl(con,dbplyr::in_schema("filter","refs"))
-raw_meta <- tbl(con,dbplyr::in_schema("filter","raw_meta"))
+register_tables(con, "filter")
+register_tables(con, "filter_statistical_overview")
 
-words <- tbl(con,dbplyr::in_schema("filter","words"))
-word_freq <- tbl(con,dbplyr::in_schema("filter","word_freq"))
-word_occ <- tbl(con,dbplyr::in_schema("filter","word_occ"))
+themes_to_top_level_themes <- tbl(con,sql("
+WITH RECURSIVE theme_ancestor AS (
+  SELECT t_id, t_id AS ancestor_t_id FROM filter.themes
+  WHERE par_id = 23959
+  UNION ALL
+  SELECT themes.t_id, ancestor_t_id
+  FROM theme_ancestor, filter.themes
+  WHERE themes.par_id=theme_ancestor.t_id
+) 
+SELECT * FROM theme_ancestor
+"))
 
 polygons <- st_read(con, query='SELECT name, ST_AsBinary(geometry) AS geometry FROM filter.polygons', geometry_column='geometry')
 st_crs(polygons) <- 'urn:ogc:def:crs:EPSG::3857'
