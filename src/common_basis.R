cb763d6e9656435d78bcc1ad16591bd8ade7563b
Eetu Mäkelä <eetu.makela@iki.fi>
2024-03-14 12:55:01 +0200

    add map experiments

--- src/common_basis.R
+++ src/common_basis.R
@@ -11,6 +11,9 @@ library(sf)
 library(tmap)
 library(ggridges)
 library(ggrepel)
+library(glue)
+library(plotly)
+library(ggforce)
 
 p <- function(number) {
   return(format(number, scientific = FALSE, big.mark = ","))
@@ -27,17 +30,21 @@ con <- get_connection()
 register_tables(con, "filter")
 register_tables(con, "filter_statistical_overview")
 
-themes_to_top_level_themes <- tbl(con,sql("
-WITH RECURSIVE theme_ancestor AS (
-  SELECT t_id, t_id AS ancestor_t_id FROM filter.themes
+types_to_top_level_types <- tbl(con,sql("
+WITH RECURSIVE type_ancestor AS (
+  SELECT t_id, t_id AS ancestor_t_id FROM filter.types
   WHERE par_id = 23959
   UNION ALL
-  SELECT themes.t_id, ancestor_t_id
-  FROM theme_ancestor, filter.themes
-  WHERE themes.par_id=theme_ancestor.t_id
+  SELECT types.t_id, ancestor_t_id
+  FROM type_ancestor, filter.types
+  WHERE types.par_id=type_ancestor.t_id
 ) 
-SELECT * FROM theme_ancestor
+SELECT * FROM type_ancestor
 "))
 
-polygons <- st_read(con, query='SELECT name, ST_AsBinary(geometry) AS geometry FROM filter.polygons', geometry_column='geometry')
-st_crs(polygons) <- 'urn:ogc:def:crs:EPSG::3857'
+polygons <- st_read(con, query="
+SELECT pol_id, ST_AsBinary(geometry) AS geometry 
+FROM filter.polygons 
+WHERE geometry IS NOT NULL", geometry_column='geometry')
+st_crs(polygons) <- 'urn:ogc:def:crs:EPSG::3067'
+polygons <- st_make_valid(polygons) %>% inner_join(map_pol %>% inner_join(maps) %>% inner_join(pol_pl) %>% collect())
